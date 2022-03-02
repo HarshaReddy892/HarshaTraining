7279a8f409f409cd13710e35d8c3eee8087d9c19
HarshaTraining <harsha.v.kasa@gmail.com>
2022-03-02 10:53:42 +0530

    commit

---
+++ EMIAngularTraining_2022-main/AngularDemo/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { DatabindingComponent } from './Components/databinding/databinding.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DatabindingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
